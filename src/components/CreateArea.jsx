f7b79543495e7d49aac9da44d53f755b359f85ac
Phyllo <firo.kong@gmail.com>
2021-09-20 09:22:08 -0500

    v0.1.0

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
+
+function CreateArea(props) {
+    const [isFocused, setFocused] = useState(false);
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    });
+
+    function handleChange(event) {
+        const {name, value} = event.target;
+
+        setNote((prevValue) => {
+            return {
+                ...prevValue,
+               [name]: value
+            }
+        });
+    }
+
+    function handleClick(event) {
+        if (note.title !== "") {
+            props.addNote(note);
+            setFocused(false);
+            setNote({title: "", content: ""});
+        }
+        event.preventDefault();
+    }
+
+    function toggleFocus() {
+        setFocused(isFocused && note.title === "" ? false : true);
+    }
+
+    return (
+        <div>
+            <form className="create-note">
+            <input onChange={handleChange} onClick={toggleFocus} name="title" value={note.title} autoComplete="off" placeholder={isFocused ? "Title" : "Take a note..."} />
+            {isFocused && <textarea onChange={handleChange} name="content" value={note.content} placeholder="Take a note..." rows="3" />}
+            {isFocused && <Zoom in={true}><Fab onClick={handleClick}><AddIcon /></Fab></Zoom>}
+            </form>
+        </div>
+    );
+}
+
+export default CreateArea;
